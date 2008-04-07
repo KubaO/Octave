44fa02879874d0ccc15784ec237007285617f3ca
John W. Eaton <jwe@octave.org>
2008-04-07 13:40:38 -0400

    update copyright dates
    --HG--
    branch : release-3-0-x

--- scripts/control/base/__stepimp__.m
+++ scripts/control/base/__stepimp__.m
@@ -1,4 +1,4 @@
-## Copyright (C) 1996, 1998, 2000, 2003, 2004, 2005, 2006, 2007
+## Copyright (C) 1996, 1998, 2000, 2003, 2004, 2005, 2006, 2007, 2008
 ##               Auburn University.  All rights reserved.
 ##
 ## This file is part of Octave.
--- scripts/general/del2.m
+++ scripts/general/del2.m
@@ -1,5 +1,5 @@
-## Copyright (C) 2000, 2007 Kai Habel
-## Copyright (C) 2007  David Bateman
+## Copyright (C) 2000, 2007, 2008 Kai Habel
+## Copyright (C) 2007, 2008  David Bateman
 ##
 ## This file is part of Octave.
 ##
